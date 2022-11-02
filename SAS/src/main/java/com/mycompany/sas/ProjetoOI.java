88d382c166388491bff9d35a71e3d761f9da9a10
ricar <ricar@PC-TAKESHI>
2022-11-02 19:07:39 -0300

    Criação das telas do projeto SAS feito pelo Luiz Henrique.

--- SAS/src/main/java/com/mycompany/sas/ProjetoOI.java
+++ SAS/src/main/java/com/mycompany/sas/ProjetoOI.java
@@ -3,8 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.utfpr.projetooi;
-
+package com.mycompany.sas;
 /**
  *
  * @author Tchuco
