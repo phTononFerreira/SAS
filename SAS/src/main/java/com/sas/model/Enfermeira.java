1e518888d64ce33457d050b6e8bd49fb1b1613aa
ViniciusGR797 <loritoamarelo@outlook.com>
2022-11-15 19:50:53 -0300

    Pesquisa enfermeira por id

--- SAS/src/main/java/com/sas/model/Enfermeira.java
+++ SAS/src/main/java/com/sas/model/Enfermeira.java
@@ -1,9 +1,13 @@
 package com.sas.model;
 
+import com.sas.controller.ConexaoBD;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+
 public class Enfermeira extends Funcionario {
 
 	private String cr;
-	private int enf_id;
+	private int adm_id;
 
 	public String getCr() {
 		return cr;
@@ -12,19 +16,19 @@ public class Enfermeira extends Funcionario {
 		this.cr = cr;
 	}
 
-	public int getEnf_id() {
-		return enf_id;
+	public int getAdm_id() {
+		return adm_id;
 	}
-	public void setEnf_id(int enf_id) {
-		this.enf_id = enf_id;
+	public void setAdm_id(int enf_id) {
+		this.adm_id = enf_id;
 	}
 
 	public Enfermeira() {
 	}
 
-	public Enfermeira(String cr, int enf_id) {
+	public Enfermeira(String cr, int adm_id) {
 		this.cr = cr;
-		this.enf_id = enf_id;
+		this.adm_id = adm_id;
 	}
 	
 	public Prontuario gerarPro() {
@@ -42,4 +46,32 @@ public class Enfermeira extends Funcionario {
 	public int contarEstoque(InsumoMedico ins) {
 		return 0;
 	}
+        
+        public static Enfermeira pesquisarEnfermeiraID(String ID) {
+        ResultSet pesquisa = ConexaoBD.getConexao().executarQueryBD("SELECT * FROM enfermeira WHERE enf_id = '" + ID + "'");
+        Enfermeira enf = null;
+        try {
+            if (pesquisa.isBeforeFirst()) {
+                enf = new Enfermeira();
+                pesquisa.next();
+                enf.setId(pesquisa.getString("enf_id"));
+                enf.setSenha(pesquisa.getString("enf_senha"));
+                enf.setNome(pesquisa.getString("enf_nome"));
+                enf.setData_nascimento(new SimpleDateFormat("yyyy-MM-dd").parse(pesquisa.getString("enf_dataNasc")));
+                enf.setCpf(pesquisa.getString("enf_cpf"));
+                enf.setEndereco(pesquisa.getString("enf_endereco"));
+                enf.setSalario(Double.parseDouble(pesquisa.getString("enf_salario")));
+                enf.setCr(pesquisa.getString("enf_salario"));
+                enf.setAdm_id(pesquisa.getString("adm_id")));
+                //System.out.println(adm.toString());
+            }else{
+                enf = null;
+            }
+
+        } catch (Exception e) {
+            System.out.println("ERRO NA FORMATAÇÃO => "+e);
+        }
+
+        return enf;
+    }
 }
